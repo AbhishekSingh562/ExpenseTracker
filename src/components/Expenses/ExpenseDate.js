ebd64557a6690677ffcb4d068a4655310b4642b8
abhishek <abhisingh562002offc@gmail.com>
2023-09-30 00:51:23 +0530

    BUGS in getFullYear, in ExpenseDate.js

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -25,8 +25,8 @@ import "./ExpenseDate.css";
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  // const year = props.date.getFullYear();
-  const year = new Date(props.date).getUTCFullYear();
+  const year = props.date.getFullYear();
+  // const year = new Date(props.date).getUTCFullYear();
 
   return (
     <div className="expense-date">
